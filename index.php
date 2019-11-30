84f753a8aa239c7d81f824e307eec414603f6aa2
thando mini <tzone85@gmail.com>
2019-11-30 03:24:58 +0200

    started the landing or index page for the products

--- index.php
+++ index.php
@@ -1,1 +1,41 @@
-testing
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PDO - Read Records - PHP CRUD Tutorial</title>
+
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+	<!--custom css-->
+	<style>
+		.m-r-1em {margin-right: 1em;}
+		.m-b-1em {margin-bottom: 1em;}
+		.m-l-1em {margin-left: 1em;}
+		.mt0 {margin-top:0;}
+	</style>
+
+</head>
+<body>
+
+	<!--container-->
+	<div class="container">
+		<div class="page-header">
+			<h1>Read Products</h1>
+		</div>
+
+		<!-- PHP code to read records will come here -->
+	</div> <!-- end .container -->
+
+	<!-- jQuery (necessary for bootstrap's js plugins) -->
+	<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+<!--	confirm delete record will be here-->
+</body>
+</html>
